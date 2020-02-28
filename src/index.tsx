e08422936eb693076dd206ce3d3c12245d813eb6
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-02-28 11:02:52 +0530

    components: implementing basic card component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { ReactNodeArray } from 'react';
+import {
+  Card, CardWrapper, FrontCardFace, BackCardFace,
+} from './styles';
 
 export const FlipCard:React.FC<{children: React.ReactChildren}> = ({
   children,
-}) => <div>{children}</div>;
+}) => {
+  if ((children as ReactNodeArray).length !== 2) {
+    throw new Error('Flip card requires exactly two children');
+  }
+
+  const frontFace = (children as ReactNodeArray)[0];
+  const backFace = (children as ReactNodeArray)[0];
+
+  return (
+    <CardWrapper>
+      <Card animationDelay="0.5">
+        <FrontCardFace>{frontFace}</FrontCardFace>
+        <BackCardFace>{backFace}</BackCardFace>
+      </Card>
+    </CardWrapper>
+  );
+};
---
+++ src/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const CardWrapper = styled.div`
+    perspective: 1000px;
+    height: 100%;
+    width: 100%;
+`;
+
+export const Card = styled('div')<{animationDelay: String}>`
+    height: 100%;
+    width: 100%;
+    transform: rotateY(0deg);
+    transform-style: preserve-3d;
+    transition: ${(props) => `transform ${props.animationDelay}s`}
+    position: relative;
+`;
+
+const CommonCardFace = styled.div`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+`;
+
+export const FrontCardFace = CommonCardFace;
+
+export const BackCardFace = styled(CommonCardFace)`
+    transform: rotateY(180deg);
+`;
